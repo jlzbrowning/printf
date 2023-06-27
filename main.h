b0fabe2acd23a9993e4d6422e40f21460272845c
jlzbrowning <jlzbrowning@gmail.com>
2023-06-27 01:44:11 -0700

    task 11

--- main.h
+++ main.h
@@ -8,7 +8,7 @@ int _putchar(char c)
     return (write(1, &c, 1));
 }
 
-int _print_int(int n, int plus_flag, int space_flag)
+int _print_int(int n, int plus_flag, int space_flag, int zero_flag, int field_width)
 {
     unsigned int m, digit, length = 0;
 
@@ -33,9 +33,18 @@ int _print_int(int n, int plus_flag, int space_flag)
         }
     }
 
+    if (zero_flag && field_width)
+    {
+        while (length < field_width)
+        {
+            _putchar('0');
+            length++;
+        }
+    }
+
     digit = m / 10;
     if (digit > 0)
-        length += _print_int(digit, 0, 0);
+        length += _print_int(digit, 0, 0, 0, 0);
 
     _putchar((m % 10) + '0');
     length++;
@@ -43,7 +52,7 @@ int _print_int(int n, int plus_flag, int space_flag)
     return (length);
 }
 
-int _print_uint(unsigned int n, int hash_flag)
+int _print_uint(unsigned int n, int hash_flag, int zero_flag, int field_width)
 {
     unsigned int digit, length = 0;
 
@@ -53,9 +62,18 @@ int _print_uint(unsigned int n, int hash_flag)
         length++;
     }
 
+    if (zero_flag && field_width)
+    {
+        while (length < field_width)
+        {
+            _putchar('0');
+            length++;
+        }
+    }
+
     digit = n / 10;
     if (digit > 0)
-        length += _print_uint(digit, 0);
+        length += _print_uint(digit, 0, 0, 0);
 
     _putchar((n % 10) + '0');
     length++;
@@ -63,7 +81,7 @@ int _print_uint(unsigned int n, int hash_flag)
     return (length);
 }
 
-int _print_octal(unsigned int n, int hash_flag)
+int _print_octal(unsigned int n, int hash_flag, int zero_flag, int field_width)
 {
     unsigned int digit, length = 0;
 
@@ -73,9 +91,18 @@ int _print_octal(unsigned int n, int hash_flag)
         length++;
     }
 
+    if (zero_flag && field_width)
+    {
+        while (length < field_width)
+        {
+            _putchar('0');
+            length++;
+        }
+    }
+
     digit = n / 8;
     if (digit > 0)
-        length += _print_octal(digit, 0);
+        length += _print_octal(digit, 0, 0, 0);
 
     _putchar((n % 8) + '0');
     length++;
@@ -83,7 +110,7 @@ int _print_octal(unsigned int n, int hash_flag)
     return (length);
 }
 
-int _print_hex(unsigned int n, int upper_case, int hash_flag)
+int _print_hex(unsigned int n, int upper_case, int hash_flag, int zero_flag, int field_width)
 {
     unsigned int digit, length = 0;
 
@@ -94,9 +121,18 @@ int _print_hex(unsigned int n, int upper_case, int hash_flag)
         length += 2;
     }
 
+    if (zero_flag && field_width)
+    {
+        while (length < field_width)
+        {
+            _putchar('0');
+            length++;
+        }
+    }
+
     digit = n / 16;
     if (digit > 0)
-        length += _print_hex(digit, upper_case, 0);
+        length += _print_hex(digit, upper_case, 0, 0, 0);
 
     _putchar(upper_case ? "0123456789ABCDEF"[n % 16] : "0123456789abcdef"[n % 16]);
     length++;
@@ -123,6 +159,7 @@ int _print_S(char *str)
             _putchar(*str);
             length++;
         }
+
         str++;
     }
 
@@ -132,12 +169,9 @@ int _print_S(char *str)
 int _printf(const char *format, ...)
 {
     va_list args;
-    int count = 0;
-    int i;
+    int count = 0, i, plus_flag = 0, space_flag = 0, hash_flag = 0, zero_flag = 0;
+    int field_width = 0, precision = -1;
     char *str;
-    int plus_flag = 0, space_flag = 0, hash_flag = 0;
-    int length_l = 0, length_h = 0;
-    int field_width = 0, precision = -1, width;
 
     va_start(args, format);
 
@@ -147,11 +181,12 @@ int _printf(const char *format, ...)
         {
             i++;
 
-            while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+            while (format[i] == '+' || format[i] == ' ' || format[i] == '#' || format[i] == '0')
             {
                 plus_flag |= format[i] == '+';
                 space_flag |= format[i] == ' ';
                 hash_flag |= format[i] == '#';
+                zero_flag |= format[i] == '0';
                 i++;
             }
 
@@ -180,12 +215,10 @@ int _printf(const char *format, ...)
                 break;
             case 's':
                 str = va_arg(args, char *);
-                width = 0;
-                while (*str && width < precision)
+                while (*str && count < precision)
                 {
                     _putchar(*str++);
                     count++;
-                    width++;
                 }
                 break;
             case '%':
@@ -194,26 +227,26 @@ int _printf(const char *format, ...)
                 break;
             case 'd':
             case 'i':
-                count += _print_int(va_arg(args, int), plus_flag, space_flag);
+                count += _print_int(va_arg(args, int), plus_flag, space_flag, zero_flag, field_width);
                 break;
             case 'u':
-                count += _print_uint(va_arg(args, unsigned int), hash_flag);
+                count += _print_uint(va_arg(args, unsigned int), hash_flag, zero_flag, field_width);
                 break;
             case 'o':
-                count += _print_octal(va_arg(args, unsigned int), hash_flag);
+                count += _print_octal(va_arg(args, unsigned int), hash_flag, zero_flag, field_width);
                 break;
             case 'x':
-                count += _print_hex(va_arg(args, unsigned int), 0, hash_flag);
+                count += _print_hex(va_arg(args, unsigned int), 0, hash_flag, zero_flag, field_width);
                 break;
             case 'X':
-                count += _print_hex(va_arg(args, unsigned int), 1, hash_flag);
+                count += _print_hex(va_arg(args, unsigned int), 1, hash_flag, zero_flag, field_width);
                 break;
             case 'S':
                 count += _print_S(va_arg(args, char *));
                 break;
             }
 
-            plus_flag = space_flag = hash_flag = length_l = length_h = field_width = 0;
+            plus_flag = space_flag = hash_flag = zero_flag = field_width = 0;
             precision = -1;
         }
         else
